9e95d5a7364c0f98c5393050c6e7c4d4a4e15f99
richdonne <rd@mymailworld.co.uk>
2014-07-25 22:52:56 +0100

    Updated to Remove ID,Created_at,Updated_at fields

--- app/commands/BuildConstructor.php
+++ app/commands/BuildConstructor.php
@@ -11,11 +11,11 @@ trait BuildConstructor {
 		$innerfields  = '';
 
 		foreach ($fields as $field) {
-
+			if(! in_array($field,['id','created_at','updated_at'])){
 			$publicfields = $publicfields . ' public $' . $field . ';  ';
 			$construct    = $construct . '$' . $field . ',';
 			$innerfields  = $innerfields . '$this->' . $field . '=' . '$' . $field . ';';
-
+			}
 		}
 
 		return array(
